c0f33d650a34e5b7cf0ea78f25018f733333a6ac
joimar755 <joimarjose19@gmail.com>
2024-11-25 15:16:37 -0500

    a

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Establece el límite en KB (aquí 1 MB)
+},
 })
